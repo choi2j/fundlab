f7b27e5887a5f302da9f45681800547dbcb36318
choi2j <choi2j20@gmail.com>
2025-02-25 22:25:52 +0900

    type update

--- src/lib/ts/type.ts
+++ src/lib/ts/type.ts
@@ -1,16 +1,52 @@
 type Research = {
-    thumbnail: string;
-    title: string;
-    who: string;
-    where: string;
-    views: number;
-    when: Date;
-    content: string;
-    summary: string;
-    tag: string[];
-}
+	thumbnail: string;
+	title: string;
+	who: string;
+	where: string;
+	views: number;
+	when: Date;
+	content: string;
+	summary: string;
+	tag: string[];
+};
+
+type Investment = {
+	thumbnail: string;
+	title: string;
+	who: string;
+	where: string;
+	views: number;
+	when: Date;
+	content: string;
+	summary: string;
+	tag: string[];
+};
 
 type filterItem = {
-    tag: string;
-    content: string;
-}
+	tag: string;
+	content: string;
+};
+
+type User = {
+	name: string;
+	where: string;
+	type: string;
+	profile: string;
+};
+
+type ProfileInfo = {
+	name: string;
+	where: string;
+	type: string;
+	profile: string;
+	summary: string;
+};
+
+type PersonalInfo = {
+	name: string;
+	where: string;
+	type: string;
+	summary?: string;
+	myWork?: Research[] | Investment[];
+	profile: string;
+};
--- src/routes/message/+page.svelte
+++ src/routes/message/+page.svelte
@@ -2,12 +2,45 @@
 	import '$lib/style.css';
 
 	import { page } from '$app/state';
-	
+
 	import Navbar from '$lib/components/Navbar.svelte';
+	import square from '$lib/assets/img/logo.png';
+
+	let chatList: User[] = $state([]);
+	chatList = [
+		{
+			name: 'Temp Gyosu',
+			where: 'Sung',
+			type: '교수',
+			profile: square
+		},
+		{
+			name: 'Temp Gyosu',
+			where: 'Sung',
+			type: '교수',
+			profile: square
+		},
+		{
+			name: 'Temp Gyosu',
+			where: 'Sung',
+			type: '교수',
+			profile: square
+		}
+	];
 
 	let pathname = $state(page.url.pathname);
 </script>
 
 <div class="fs hori">
 	<Navbar {pathname} />
+	<div class="fs hori padding-3">
+		<div class="width-30p vert gap-3 shadow">
+			{#each chatList as chats}
+				<button class="padding-1 hori dark1-border-1 noradius center-a">
+					<p class="regular-t medi-t">{chats.name}</p>
+				</button>
+			{/each}
+		</div>
+		<div class="width-70p vert"></div>
+	</div>
 </div>
